493fa476463314c22df665638f189692c5e70fab
Jose Miguel <jose.miguelq95@gmail.com>
2017-10-01 19:10:45 -0400

    key too long fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1"> 
-            <meta name="description" content="Ciclo Grande - Bicicletas compartilhadas">
+            <meta name="WEBSITE-DESCRIPTION" content="Ciclo Grande - Bicicletas compartilhadas">
             <title>Ciclo Grande</title> 
             
             <link rel="shortcut icon" href="landing/images/favicon.ico">
